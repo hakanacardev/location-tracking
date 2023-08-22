7e4b9a6b82168c2c63d17af6731f195d35d09331
hakanacardev <hakanacar200@gmail.com>
2023-08-22 15:14:57 +0300

    bottom tab style

--- locationTracking/src/pages/BottomTab.tsx
+++ locationTracking/src/pages/BottomTab.tsx
@@ -1,15 +1,50 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Maps from './Maps';
 import MyAccount from './MyAccount';
-
+import {Image, View} from 'react-native';
+import MyAccountImage from '../assets/MyAccount.png';
+import Gps from '../assets/Gps.png';
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+          elevation: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }}>
       <Tab.Screen
         options={{
           headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View
+              style={{
+                backgroundColor: focused ? '#4D4D4D' : '#FFFFFF',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 'auto',
+                padding: 10,
+                width: '100%',
+                borderTopColor: 'rgba(77, 77, 77, 0.5)',
+              }}>
+              <Image
+                source={Gps}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 35,
+                  tintColor: focused ? '#FFFFFF' : '#4D4D4D',
+                }}
+              />
+              {/*  <Text style={{ color: focused ? '#FFFFFF' : '#3C3C3B', }}>{t("anasayfa")}</Text> */}
+            </View>
+          ),
         }}
         name="Home"
         component={Maps}
@@ -17,8 +52,31 @@ function MyTabs() {
       <Tab.Screen
         options={{
           headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View
+              style={{
+                backgroundColor: focused ? '#4D4D4D' : '#FFFFFF',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 'auto',
+                padding: 10,
+                width: '100%',
+                borderTopColor: 'rgba(77, 77, 77, 0.5)',
+              }}>
+              <Image
+                source={MyAccountImage}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 35,
+                  tintColor: focused ? '#FFFFFF' : '#4D4D4D',
+                }}
+              />
+              {/*  <Text style={{ color: focused ? '#FFFFFF' : '#3C3C3B', }}>{t("anasayfa")}</Text> */}
+            </View>
+          ),
         }}
-        name="Settings"
+        name="MyAccount"
         component={MyAccount}
       />
     </Tab.Navigator>
